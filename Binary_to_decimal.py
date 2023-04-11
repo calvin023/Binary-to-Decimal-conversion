b171e1873679e15efa6065c4be6a6719c96c1f82
Calvin <125489822+calvin023@users.noreply.github.com>
2023-04-11 11:56:02 +0200

    Update Binary_to_decimal.py
    Removed the useless while function and added comments

--- Binary_to_decimal.py
+++ Binary_to_decimal.py
@@ -1,11 +1,12 @@
-while True:
-    binnumber = input("Enter your binary number: ")
-    if not all(c.isdigit() and int(c) < 2 for c in binnumber):
-        raise ValueError("Invalid binary number")
-    else:
-        binlist = list(binnumber)
-        decimal = 0
-        for i in range(len(binlist)):
-            decimal += int(binlist[i]) * 2**(len(binlist)-i-1)
-        print(decimal)
-        break
+binnumber = input("Enter your binary number: ")
+# Checking that the input is a binary number
+if not all(c.isdigit() and int(c) < 2 for c in binnumber):
+    raise ValueError("Invalid binary number")
+else:
+    # Setting up variables
+    binlist = list(binnumber)
+    decimal = 0
+    # Converting to decimal
+    for i in range(len(binlist)):
+        decimal += int(binlist[i]) * 2**(len(binlist)-i-1)
+    print(decimal)
